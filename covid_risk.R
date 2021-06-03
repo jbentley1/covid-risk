1bdfa09956520d74c287a885c300bfb241c134a2
jbentley1 <65326543+jbentley1@users.noreply.github.com>
2021-06-03 09:27:33 -0400

    Add files via upload

--- covid_risk.R
+++ covid_risk.R
@@ -1,48 +1,31 @@
-library(ggplot2)
 
-currentNewCasesPerDay = 200
-population = 6893000
+currentNewCasesPerDay = 220 #Current as of 6/1/21
+population = 6893000 #MA population
 daysContagious = 10
+riskofDeathFromDrivingOneMile <- (150 / 10000000000) #150 fatalities per 10 billion miles
+toddlerRiskOfDeathPerInfection <- 4/88089 #Rough, from https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/COVID-19/COVID-19-Cases-by-Age-Group.aspx
+chanceOfBreakthroughInfection <- 0.05 #Based on 95% Moderna efficacy, assumes that exposure without vaccine would be a 100% chance of infection
+chanceOfInfectedParentTransmittingToToddler <- 1.0
+
+##Assumptions:
+# 1. The relative overall risk of serious adverse events from toddlers getting COVID vs driving is the same as the relative risk of death from driving and COVID
+# 2. MA numbers are accurate. For safety, you could multiply by a factor of 10 to get a reasonable upper bound on true current infections
+
 
 totalContagiousIndividuals <- currentNewCasesPerDay * daysContagious
 chanceOfIndividualBeingContagious <- totalContagiousIndividuals / population #This seems to be ~2x as conservative as the method MA uses to estimate total active cases
-
-riskTable <- data.frame(attendees=1:70)
-riskTable$chanceOfAtLeastOneContagiousAttendee <- 1-((1-chanceOfIndividualBeingContagious)^riskTable$attendees)
-riskTable$expectedInfections <- riskTable$chanceOfAtLeastOneContagiousAttendee * riskTable$attendees
-
-print(riskTable %>% mutate(percChanceOfAtLeastOneContagiousAttendee = chanceOfAtLeastOneContagiousAttendee*100))
-
-ggplot(riskTable, aes(x=attendees, y=chanceOfAtLeastOneContagiousAttendee)) + geom_point() + geom_line()  + xlab("Number of people invited")
-
-ggplot(riskTable, aes(x=attendees, y=expectedInfections)) +
-  geom_line() +
-  ylab("Expected number of new infections") +
-  xlab("Number of attendees") +
-  ggtitle("Expected number of new COVID infections when attending a party where each attendee coughs in the mouth of every other attendee as of 7/6/2020")
+riskOfDeathFromSinglePersonExposureOfVaccinatedPersonToToddler <- chanceOfIndividualBeingContagious * chanceOfBreakthroughInfection * toddlerRiskOfDeathPerInfection
 
 
 
-toddlerRiskOfDeathPerInfection <- 4/88089 #Rough, from https://www.cdph.ca.gov/Programs/CID/DCDC/Pages/COVID-19/COVID-19-Cases-by-Age-Group.aspx
-
-riskOfDeathFromSinglePersonExposureOfVaccinatedPersonToToddler <- chanceOfIndividualBeingContagious * 0.05 * toddlerRiskOfDeath
-
-riskofDeathFromDrivingOneMile <- (150 / 10000000000) #150 fatalities per 10 billion miles
-
-
+##Charting:
+library(ggplot2)
 toddlerRiskTable <- data.frame(groups=c("Risk to Toddler From Vaccinated Parent Being Exposed to Random MA Individual", "Risk from Driving 1 Mile"),
-                        risk=c(riskOfDeathFromSinglePersonExposureOfVaccinatedPersonToToddler*100, riskofDeathFromDrivingOneMile*100))
+                               risk=c(riskOfDeathFromSinglePersonExposureOfVaccinatedPersonToToddler*100, riskofDeathFromDrivingOneMile*100))
 
 ggplot(toddlerRiskTable, aes(x=groups, y=risk)) +
   geom_bar(stat="identity") +
   scale_y_continuous(labels = scales::percent) +
-  geom_text(aes(label=scales::percent(risk)), position=position_dodge(width=0.9), vjust=-0.25)
+  geom_text(aes(label=scales::percent(risk)), position=position_dodge(width=0.9), vjust=-0.25) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
-
-##Assumptions:
-# 1. If a parent gets COVID, there is a 100% chance the toddler will get it
-# 2. The relative overall risk from toddlers getting COVID is the same as the relative risk of death from driving and COVID
-# 3. Assumes that the chance of a vaccinated person getting COVID from an exposure is 5% (this assumes it's such a strong exposure they would have gotten it if not vaccinated)
-# 4. MA numbers are accurate
-
-## All except (4) make the data skew towards COVID being less dangerous than the model suggests. Possibly scale risk 10x to compensate for (4).
